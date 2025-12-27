ac7f188b039559ba0c6c158a61f9c28ee46ac730
kumar <kumar.rit8@gmail.com>
2025-12-27 13:03:56 +0530

    server commit api.ts changed

--- quickbill/services/api.ts
+++ quickbill/services/api.ts
@@ -3,7 +3,7 @@ import { Party, Item, Invoice, Payment, Expense, User } from "../types";
 // ========================
 // CONFIG
 // ========================
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_BASE = `${API_BASE_URL}/api`;
  // change if needed
 
